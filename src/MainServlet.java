6ba50abb9e3b140119e385330a488ae0ff69bc9a
tarasrakochy <tarkorit911@gmail.com>
2015-07-14 11:34:37 +0300

    created servlet

---
+++ src/MainServlet.java
@@ -0,0 +1,20 @@
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+@WebServlet("/servlett")
+public class MainServlet extends HttpServlet {
+ 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+    	PrintWriter out = resp.getWriter();
+    	out.print("<h1>Hello, servlet!!!</h1>");
+    }
+ 
+}
